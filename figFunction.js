f877d61f971c1c628388bac37b4e0881a8846157
royquillca <ccus.andy.quillca@gmail.com>
2021-10-08 18:54:20 -0500

    a

--- figFunction.js
+++ figFunction.js
@@ -1,9 +1,45 @@
 // Codigo del Cuadrado
+console.group("Cuadrado");
+
 function areaCuadrado(lado){
+    // this.areaCuadrado = areaCuadrado;
     return lado*lado;
-}
+};
 
 function perimetroCuadrado(lado) {
+    // this.perimetroCuadrado=this.perimetroCuadrado;
     return 4*lado;
+};
+// console.log(`El cuadrado de lado ${this.lado} tiene: \nPerímetro igual a ${this.perimetroCuadrado} \nÁrea igual a: ${this.areaCuadrado}cm^2`);
+console.groupEnd();
+
+//Codigo del Triángulo
+
+console.group("Triangulo");
+
+function perimetroTriangulo(lado1, lado2, base) {
+    return lado1 + lado2 + base;
 }
-// document.write(`El cuadrado de lado ${lado} tiene: \nPerímetro igual a ${perimetroCuadrado} \nÁrea igual a: ${areaCuadrado}cm^2`);
+
+function areaTriangulo(base, altura) {
+    return (base*altura)*0.5;
+}
+
+console.groupEnd();
+
+//Codigo del Circulo
+
+console.group("Circulo");
+
+function diametroCirculo(radio) {
+    return 2*radio;
+}
+function perimetroCirculo(radio) {
+    const diametro = diametroCirculo(radio);
+    return diametro*Math.PI;
+}
+function areaCirculo(radio) {
+     return Math.PI*Math.pow(radio,2);
+ }
+// console.log("El area del circulo de radio igual a " + radio + ", tiene un perímetro de "+ perimetroCirculo + ", y un área de "+ areaCirculo );
+console.groupEnd();
