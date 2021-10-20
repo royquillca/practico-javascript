f8350877643b642c51f079d3669c1dfac22c5688
royquillca <ccus.andy.quillca@gmail.com>
2021-10-20 05:39:37 -0500

    Añadí el reto del Calculo de Perimetro y Area

--- figFunction.js
+++ figFunction.js
@@ -46,11 +46,45 @@ console.groupEnd();
 
 
 // Aqui empezamos a interactuar con el HTML figuras.html
-function calcularPerimetroCuadrado(params) {
-    const input = document.getElementById("InputCuadrado");
-    const value = input.value;
-    
+
+/**CUADRADO */
+function calcularPerimetroCuadrado() {
+    const input = document.getElementById("InputCuadrado"); //Llamando al valor que escribirán los usuarios desde el html
+    const value = input.value; //Solo se necesita su valor por ello se usa el input.value;
+    const perimetro = perimetroCuadrado(value);
+    alert(perimetro);
 }
-function calcularAreaCuadrado(params) {
-    
+function calcularAreaCuadrado() {
+    const entrada = document.getElementById("InputCuadrado")
+    const value = entrada.value;
+    const area = areaCuadrado(value);
+    alert(area);
+}
+
+/**TRIANGULO */
+function calcularPerimetroTriangulo() {
+    const value1 = parseFloat(document.getElementById("InputLado1").value);
+    const value2 = parseFloat(document.getElementById("InputLado2").value);
+    const value3 = parseFloat(document.getElementById("InputLado3").value);
+    const perimeter = perimetroTriangulo(value1, value2, value3);
+    alert(perimeter);
+}
+
+function calcularAreaTriangulo() {
+    const base = parseFloat(document.getElementById("InputBase").value);
+    const altura = parseFloat(document.getElementById("InputAltura").value);
+    const area = areaTriangulo(base, altura);
+    alert(area);
+}
+
+/** CIRCULO */
+function calcularPerimetroCirculo() {
+    const radio = parseFloat(document.getElementById("InputRadio").value);
+    const perimetro = perimetroCirculo(radio);
+    alert(perimetro)
+}
+function calcularAreaCirculo() {
+    const radio = parseFloat(document.getElementById("InputRadio").value);
+    const area = areaCirculo(radio);
+    alert(area);
 }
